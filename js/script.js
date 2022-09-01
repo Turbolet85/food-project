a9e6374649f6bb5a054f88368b94934f7e63706d
Anton Soldatov <turbolet85@gmail.com>
2022-09-01 19:53:13 +0200

    calculator done

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	//////////////////////////////////////////////////timer
 
-	const deadLine = '2022-08-31';
+	const deadLine = '2022-09-30';
 	function getTimeRemaining(endtime) {
 		let days, hours, minutes, seconds;
 		const t = Date.parse(endtime) - Date.parse(new Date());
@@ -120,10 +120,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 
 	modal.addEventListener('click', (event) => {
-		if (
-			event.target === modal ||
-			event.target.getAttribute('data-close') == ''
-		) {
+		if (event.target === modal || event.target.getAttribute('data-close') == '') {
 			closeModal();
 		}
 	});
@@ -137,10 +134,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	const modalTimerId = setTimeout(showModal, 50000);
 
 	function showModalByScroll() {
-		if (
-			window.scrollY + document.documentElement.clientHeight >=
-			document.documentElement.scrollHeight - 1
-		) {
+		if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
 			showModal();
 			window.removeEventListener('scroll', showModalByScroll);
 		}
@@ -213,14 +207,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	axios.get('http://localhost:3000/menu').then((data) =>
 		data.data.forEach(({ img, altimg, title, descr, price }) => {
-			new MenuCard(
-				img,
-				altimg,
-				title,
-				descr,
-				price,
-				'.menu .container'
-			).render();
+			new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
 		})
 	);
 
@@ -346,11 +333,15 @@ window.addEventListener('DOMContentLoaded', () => {
 		dots.push(dot);
 	}
 
+	function deleteNotDigits(str) {
+		return +str.replace(/\D/g, '');
+	}
+
 	next.addEventListener('click', () => {
-		if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+		if (offset == deleteNotDigits(width) * (slides.length - 1)) {
 			offset = 0;
 		} else {
-			offset += +width.slice(0, width.length - 2);
+			offset += deleteNotDigits(width);
 		}
 		slidesField.style.transform = `translateX(${-offset}px)`;
 
@@ -372,9 +363,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	prev.addEventListener('click', () => {
 		if (offset == 0) {
-			offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+			offset = deleteNotDigits(width) * (slides.length - 1);
 		} else {
-			offset -= +width.slice(0, width.length - 2);
+			offset -= deleteNotDigits(width);
 		}
 		slidesField.style.transform = `translateX(${-offset}px)`;
 
@@ -398,7 +389,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			const slideTo = e.target.getAttribute('data-slide-to');
 
 			slideIndex = slideTo;
-			offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+			offset = deleteNotDigits(width) * (slideTo - 1);
 			slidesField.style.transform = `translateX(${-offset}px)`;
 
 			if (slides.length < 10) {
@@ -412,7 +403,112 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 
-	//////////////////////////////////////////////////
+	////////////////////////////////////////////////// calc
+
+	const result = document.querySelector('.calculating__result span');
+
+	let sex, height, weight, age, ratio;
+
+	if (localStorage.getItem('sex')) {
+		sex = localStorage.getItem('sex');
+	} else {
+		sex = 'female';
+		localStorage.setItem('sex', 'female');
+	}
+
+	if (localStorage.getItem('ratio')) {
+		ratio = localStorage.getItem('ratio');
+	} else {
+		ratio = 1.375;
+		localStorage.setItem('ratio', 1.375);
+	}
+
+	function initLocalSettings(selector, activeClass) {
+		const elements = document.querySelectorAll(selector);
+
+		elements.forEach((elem) => {
+			elem.classList.remove(activeClass);
+			if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+				elem.classList.add(activeClass);
+			}
+			if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+				elem.classList.add(activeClass);
+			}
+		});
+	}
+
+	initLocalSettings('#gender div', 'calculating__choose-item_active');
+	initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
+	function calcTotal() {
+		if (!sex || !height || !weight || !age || !ratio) {
+			result.textContent = '____';
+			return;
+		}
+
+		if (sex === 'female') {
+			result.textContent = Math.round((447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio);
+		} else {
+			result.textContent = Math.round((88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio);
+		}
+	}
+
+	calcTotal();
+
+	function getStaticInformation(selector, activeClass) {
+		const elements = document.querySelectorAll(selector);
+
+		elements.forEach((elem) => {
+			elem.addEventListener('click', (e) => {
+				if (e.target.getAttribute('data-ratio')) {
+					ratio = +e.target.getAttribute('data-ratio');
+					localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
+				} else {
+					sex = e.target.getAttribute('id');
+					localStorage.setItem('sex', e.target.getAttribute('id'));
+				}
+
+				elements.forEach((elem) => {
+					elem.classList.remove(activeClass);
+				});
+
+				e.target.classList.add(activeClass);
+				calcTotal();
+			});
+		});
+	}
+
+	getStaticInformation('#gender div', 'calculating__choose-item_active');
+	getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
+
+	function getDynamicInformation(selector) {
+		const input = document.querySelector(selector);
+
+		input.addEventListener('input', () => {
+			if (input.value.match(/\D/g)) {
+				input.style.border = '1px solid red';
+			} else {
+				input.style.border = 'none';
+			}
+
+			switch (input.getAttribute('id')) {
+				case 'height':
+					height = +input.value;
+					break;
+				case 'weight':
+					weight = +input.value;
+					break;
+				case 'age':
+					age = +input.value;
+					break;
+			}
+			calcTotal();
+		});
+	}
+
+	getDynamicInformation('#height');
+	getDynamicInformation('#weight');
+	getDynamicInformation('#age');
 
 	//////////////////////////////////////////////////
 });
